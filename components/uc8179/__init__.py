a0a0ea5fe5675e6b4772067190cd323aa7b3858c
tetele <tm.sandu@gmail.com>
2025-08-18 20:21:31 +0000

    Add SPI config

--- components/uc8179/__init__.py
+++ components/uc8179/__init__.py
@@ -1,18 +1,34 @@
+from esphome import pins
 import esphome.codegen as cg
+from esphome.components import spi
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import CONF_BUSY_PIN, CONF_DC_PIN, CONF_ID, CONF_RESET_PIN
 
 uc8179_ns = cg.esphome_ns.namespace("uc8179")
 UC8179 = uc8179_ns.class_("UC8179", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(UC8719),
+        cv.GenerateID(): cv.declare_id(UC8179),
+        cv.Required(CONF_DC_PIN): pins.gpio_output_pin_schema,
+        cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
+        cv.Optional(CONF_BUSY_PIN): pins.gpio_input_pin_schema,
     }
-)
+).extend(spi.spi_device_schema())
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
 
     await cg.register_component(var, config)
+    await spi.register_spi_device(var, config)
+
+    dc = await cg.gpio_pin_expression(config[CONF_DC_PIN])
+    cg.add(var.set_dc_pin(dc))
+
+    if CONF_RESET_PIN in config:
+        reset = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
+        cg.add(var.set_reset_pin(reset))
+    if CONF_BUSY_PIN in config:
+        reset = await cg.gpio_pin_expression(config[CONF_BUSY_PIN])
+        cg.add(var.set_busy_pin(reset))
