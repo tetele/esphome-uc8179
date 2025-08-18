ae9e962a2d7be2f34cb9fa0dd757394317e39c0d
tetele <tm.sandu@gmail.com>
2025-08-18 20:04:18 +0000

    Convert to SPI device

--- components/uc8179/uc8179.h
+++ components/uc8179/uc8179.h
@@ -1,27 +1,28 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/components/spi/spi.h"
 
 namespace esphome
 {
 namespace uc8719
 {
 
-class UC8179 : public Component
+class UC8179 : public Component, public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_2MHZ>
 {
 public:
     void setup() override;
     void loop() override;
     void dump_config() override;
 
-    void set_foo(bool foo) { this->foo_ = foo; }
-    void set_bar(std::string bar) { this->bar_ = bar; }
-    void set_baz(int baz) { this->baz_ = baz; }
+    void set_dc_pin(GPIOPin *dc_pin) { this->dc_pin_ = dc_pin; }
+    void set_busy_pin(GPIOPin *busy_pin) { this->busy_pin_ = busy_pin; }
+    void set_reset_pin(GPIOPin *reset_pin) { this->reset_pin_ = reset_pin; }
 
 protected:
-    bool foo_{false};
-    std::string bar_{};
-    int baz_{0};
+    GPIOPin *dc_pin_{nullptr};
+    GPIOPin *busy_pin_{nullptr};
+    GPIOPin *reset_pin_{nullptr};
 };
 
 } // namespace uc8719
