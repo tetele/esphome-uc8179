7c3e5cc4635562994868a5c83f77561a0d391977
tetele <tm.sandu@gmail.com>
2025-08-19 07:14:32 +0000

    Add PWR (power setting) command

--- components/uc8179/uc8179.h
+++ components/uc8179/uc8179.h
@@ -73,6 +73,74 @@ typedef enum { // Soft Reset
     PSR_RST_N_RESET = 0x00,     // Reset. Booster OFF, Register data are set to their default values, all drivers will be reset, and all functions will be disabled. Source/Gate/Border/VCOM will be released to floating
     PSR_RST_N_NO_EFFECT = 0x01, // No effect (Default).
 } PSR_RST_N;
+
+// POWER SETTING (PWR) (R01H)
+typedef enum { // Border LDO enable
+    PWR_BD_EN_OFF = 0x00,   // Border LDO disable (Default)
+                            // Border level selection: 00b: VCOM 01b: VDH 10b: VDL 11b: VDHR
+    PWR_BD_EN_ON = 0x10,    //  Border LDO enable
+                            // Border level selection: 00b: VCOM 01b: VBH(VCOM-VDL) 10b:VBL(VCOM-VDH) 11b: VDHR
+} PWR_BD_EN;
+
+typedef enum { // Source LV power selection
+    PWR_VSR_EN_EXTERNAL = 0x00, // External source power from VDHR pins
+    PWR_VSR_EN_INTERNAL = 0x04, // Internal DC/DC function for generating VDHR. (Default)
+} PWR_VSR_EN;
+
+typedef enum { // Source power selection
+    PWR_VS_EN_EXTERNAL = 0x00,  // External source power from VDH/VDL pins
+    PWR_VS_EN_INTERNAL = 0x02,  // Internal DC/DC function for generating VDH/VDL. (Default)
+} PWR_VS_EN;
+
+typedef enum { // Gate power selection
+    PWR_VG_EN_EXTERNAL = 0x00,  // External gate power from VGH/VGL pins
+    PWR_VG_EN_INTERNAL = 0x01,  // Internal DC/DC function for generating VGH/VGL. (Default)
+} PWR_VG_EN;
+
+typedef enum {
+    PWR_VCOM_SLEW_DEFAULT = 0x10,    // VCOM slew rate selection for voltage transition. The value is fixed at “1”.
+} PWR_VCOM_SLEW;
+
+typedef enum { // VGH / VGL Voltage Level selection.
+    PWR_VG_LVL_9  = 0x00,   // VGH= 9V, VGL= -9V
+    PWR_VG_LVL_10 = 0x01,   // VGH= 10V, VGL= -10V
+    PWR_VG_LVL_11 = 0x02,   // VGH= 11V, VGL= -11V
+    PWR_VG_LVL_12 = 0x03,   // VGH= 12V, VGL= -12V
+    PWR_VG_LVL_17 = 0x04,   // VGH= 17V, VGL= -17V
+    PWR_VG_LVL_18 = 0x05,   // VGH= 18V, VGL= -18V
+    PWR_VG_LVL_19 = 0x06,   // VGH= 19V, VGL= -19V
+    PWR_VG_LVL_20 = 0x07,   // VGH= 20V, VGL= -20V
+} PWR_VG_LVL;
+
+typedef enum { // Internal VDH power selection for K/W pixel.(Default value: 111010b)
+    PWR_VDH_LVL_2_4 = 0b00000000,   // min
+    PWR_VDH_LVL_3_6 = 0b00000110,
+    PWR_VDH_LVL_5 = 0b00001101,
+    PWR_VDH_LVL_9 = 0b00100010,
+    PWR_VDH_LVL_12 = 0b00110000,
+    PWR_VDH_LVL_14 = 0b00111010,    // default
+    PWR_VDH_LVL_15 = 0b00111111,    // max
+} PWR_VDH_LVL;
+
+typedef enum { // Internal VDL power selection for K/W pixel. (Default value: 111010b)
+    PWR_VDL_LVL_2_4 = 0b00000000,   // max (negative)
+    PWR_VDL_LVL_3_6 = 0b00000110,
+    PWR_VDL_LVL_5 = 0b00001101,
+    PWR_VDL_LVL_9 = 0b00100010,
+    PWR_VDL_LVL_12 = 0b00110000,
+    PWR_VDL_LVL_14 = 0b00111010,    // default
+    PWR_VDL_LVL_15 = 0b00111111,    // min (negative)
+} PWR_VDL_LVL;
+
+typedef enum { // Internal VDHR power selection for Red pixel. (Default value: 000011b)
+    PWR_VDHR_LVL_2_4 = 0b00000000,   // min
+    PWR_VDHR_LVL_3 = 0b00000011,     // default
+    PWR_VDHR_LVL_3_6 = 0b00000110,
+    PWR_VDHR_LVL_5 = 0b00001101,
+    PWR_VDHR_LVL_9 = 0b00100010,
+    PWR_VDHR_LVL_12 = 0b00110000,
+    PWR_VDHR_LVL_15 = 0b00111111,    // max
+} PWR_VDHR_LVL;
 class UC8179 : public UC8179Base
 {
 public:
@@ -81,6 +149,15 @@ public:
     void cmd_panel_setting(PSR_REG lut_location, PSR_KWR kwr, PSR_UD gate_scan_dir, PSR_SHL source_shift_dir, PSR_SHD_N booster_switch, PSR_RST_N reset) {
         this->command(0x00);
         this->data((uint8_t)lut_location | (uint8_t)kwr | (uint8_t)gate_scan_dir | (uint8_t)source_shift_dir | (uint8_t)booster_switch | (uint8_t)reset);
+    }
+
+    void cmd_power_setting(PWR_BD_EN border_ldo, PWR_VSR_EN source_lv_power, PWR_VS_EN source_power, PWR_VG_EN gate_power, PWR_VCOM_SLEW slew_rate, PWR_VG_LVL voltage_level, PWR_VDH_LVL vdh_power, PWR_VDL_LVL vdl_power, PWR_VDHR_LVL vdhr_power) {
+        this->command(0x01);
+        this->data((uint8_t)border_ldo | (uint8_t)source_lv_power | (uint8_t)source_power | (uint8_t)gate_power);
+        this->data((uint8_t)slew_rate | (uint8_t)voltage_level);
+        this->data(vdh_power);
+        this->data(vdl_power);
+        this->data(vdhr_power);
     }
 
 
